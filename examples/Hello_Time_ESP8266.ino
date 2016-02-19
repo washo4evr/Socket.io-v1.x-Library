a6298e99fc116b9d73464a959571c0ba91d2418f
washo4evr <contact@fvidal.fr>
2016-02-19 12:09:28 -0800

    Update Hello_Time_ESP8266.ino
    header updated

--- examples/Hello_Time_ESP8266.ino
+++ examples/Hello_Time_ESP8266.ino
@@ -1,8 +1,5 @@
 /*
- *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
- *
- *  You need to get streamId and privateKey at data.sparkfun.com and paste them
- *  below. Or just customize this script to talk to other HTTP servers.
+ *  This sketch was modified to send and receive to a socket.io server
  *
  */
 #include <ESP8266WiFi.h>
